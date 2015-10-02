f367e76efe02e41b7db898316b4318b70eccc290
Mikhail Klementyev <jollheef@riseup.net>
2015-10-03 02:20:29 +0300

    Add bear integrity check

--- honeyclicker/src/honeyclicker/checker/commands.go
+++ honeyclicker/src/honeyclicker/checker/commands.go
@@ -12,10 +12,14 @@ package main
 
 import (
 	"bufio"
+	"crypto/md5"
 	"crypto/rsa"
+	"encoding/hex"
 	"fmt"
 	"golang.org/x/net/websocket"
+	"io/ioutil"
 	"net"
+	"net/http"
 )
 
 func svc_put(key *rsa.PrivateKey, host string, port int, flag string) (
@@ -113,6 +117,32 @@ func svc_chk(host string, port int) (status ServiceState, err error) {
 	}
 
 	defer ws.Close()
+
+	// Bear integrity check
+	response, err := http.Get(origin)
+	if err != nil {
+		status = STATUS_DOWN
+		err = nil
+		return
+	}
+
+	defer response.Body.Close()
+
+	contents, err := ioutil.ReadAll(response.Body)
+	if err != nil {
+		status = STATUS_MUMBLE
+		err = nil
+		return
+	}
+
+	hasher := md5.New()
+	hasher.Write(contents)
+	md5 := hex.EncodeToString(hasher.Sum(nil))
+
+	if md5 != "a60532b45c4780ba5929346bf9c7a1b4" {
+		status = STATUS_MUMBLE
+		return
+	}
 
 	return
 }
